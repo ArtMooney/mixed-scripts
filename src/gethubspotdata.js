dab95499bd360e8c6106ffe3dc9ad05226e0ed6d
Henrik Skoglund <henrik.skoglund@framecore.se>
2023-02-20 16:43:44 +0100

    updated to use attributes

--- src/gethubspotdata.js
+++ src/gethubspotdata.js
@@ -1,25 +1,38 @@
 function getHubspotData(formName) {
+  var readCookie = (function () {
+    return {
+      getCookie: function (name) {
+        var value = "; " + document.cookie;
+        var parts = value.split("; " + name + "=");
+        if (parts.length === 2) {
+          return parts.pop().split(";").shift();
+        }
+      },
+    };
+  })();
+
   if (document.getElementById(formName)) {
     var formId = document.getElementById(formName);
 
     var requestOptions = {
       method: "GET",
-      redirect: "follow"
+      redirect: "follow",
     };
 
     fetch("https://api.ipify.org?format=json", requestOptions)
       .then((response) => response.json())
       .then((result) => {
-        formId.clientip.value = result.ip;
-        formId.pageuri.value = window.location.href;
-        formId.pagename.value = document.title;
+        formId.querySelector("[clientip]").value = result.ip;
+        formId.querySelector("[pageuri]").value = window.location.href;
+        formId.querySelector("[pagename]").value = document.title;
       })
       .catch((error) => console.log("error", error));
 
     if (readCookie.getCookie("hubspotutk") !== undefined) {
-      formId.hubspotutk.value = readCookie.getCookie("hubspotutk");
+      formId.querySelector("[hubspotutk]").value =
+        readCookie.getCookie("hubspotutk");
     } else {
-      formId.hubspotutk.value = "";
+      formId.querySelector("[hubspotutk]").value = "";
     }
   }
 }
