c21dfb895ecc860018748a5067b91bc33b508c8e
Henrik Skoglund <henrik.skoglund@framecore.se>
2023-04-28 15:43:27 +0200

    removed osonerscript

--- src/osoner-wpml-fix.js
+++
@@ -1,6 +0,0 @@
-const menuLinks = document.getElementsByClassName("menu-link");
-
-for (const link of menuLinks) {
-  if (link.innerText === `' );">`)
-    link.innerHTML = link.innerHTML.replace(`' );"&gt;`, "");
-}
